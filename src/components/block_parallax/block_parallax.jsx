e9c959b848ccf0ab5d8eaa28188b5a75ed53bd16
Olifem <olifemb@gmail.com>
2024-05-13 23:14:57 +0300

    ready parallax

--- src/components/block_parallax/block_parallax.jsx
+++ src/components/block_parallax/block_parallax.jsx
@@ -1,19 +1,37 @@
 'use client'
 
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
-import { layersFooter, layersHeader, section1, section2 } from '@/components/block_parallax/data'
+import { layers, layersFooter, layersHeader, section1, section2 } from '@/components/block_parallax/data'
 import Title from '@/shares/svgs/title.svg'
-import { Card } from '@/components/card/card'
+import TitleMobile from '@/shares/svgs/title-mobile.svg'
 import Image from 'next/image'
 
-const classes = 'container grid grid-cols-2 mx-auto flex flex-row gap-8 items-center justify-center h-full'
+const classes = {
+	layer: 'container grid grid-cols-2 mx-auto flex flex-row gap-8 items-center justify-center h-full relative',
+	text: 'px-6 py-4 rounded-lg text-white text-2xl font-bold drop-shadow-md',
+}
 
 export const BlockParallax = () => {
 	const parallax = useRef(null)
+	const [current, setCurrent] = useState(0)
+
+	useEffect(() => {
+
+		console.log(parallax.current.offset)
+	}, [current])
+
+	const onScroll = (i) => {
+		setCurrent(i)
+		parallax.current.scrollTo(i)
+	}
 
 	return (
-		<Parallax ref={parallax} pages={10} style={{ top: '200', left: '0' }} className="animation">
+		<Parallax
+			ref={parallax}
+			pages={10}
+			className='animation bg h-screen !overflow-hidden'
+		>
 			{layersHeader.map((item, index) => (
 				<ParallaxLayer offset={item.offset} speed={item.speed} key={'parallaxLayer' + index}>
 					<div className={'animation_layer parallax ' + `${item.id}`} id={item.id} />
@@ -29,140 +47,63 @@ export const BlockParallax = () => {
 					justifyContent: 'center',
 				}}
 			>
-				<Title onClick={() => parallax.current.scrollTo(1)} />
-			</ParallaxLayer>
-
-			{/* SECTION 1 ------------------------------------------- */}
-			<ParallaxLayer
-				sticky={{ start: 1, end: 3.5 }}
-				style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}
-				onClick={() => parallax.current.scrollTo(3)}
-			>
-				<div className={classes}>
-					<Card className={'px-6 py-4 rounded-lg text-white text-2xl font-bold drop-shadow-md'}>
-						<div>Добро пожаловать в единственный в мире интерактивный МУЗЕЙ КОРЮШКИ!</div>
-						<br />
-						<div>
-							<span>В Музее вы узнаете:</span>
-							<ul className={''}>
-								<li> - О родственниках корюшки</li>
-								<li> - О её друзьях и недругах</li>
-								<li> - О том, что она любит и чему радуется</li>
-							</ul>
-						</div>
-						<br />
-						<div>
-							В пространстве музея вас встретят уникальные экспонаты, с которыми вы можете
-							взаимодействовать. В этом вам помогут аудио сопровождение и видео подсказки.
-						</div>
-					</Card>
+				<div onClick={() => onScroll(1)}>
+					<div className={'hidden sm:block'}><Title /></div>
+					<div className={'sm:hidden block'}><TitleMobile /></div>
 				</div>
 			</ParallaxLayer>
 
-			{section1.map(item => (
-				<ParallaxLayer speed={item.speed} offset={item.offset} sticky={item.sticky} key={item.id}>
-					<div className={classes}>
-						<div />
-						<Card className={'flex items-center justify-center'}>
-							<Image src={item.src} alt={''} width={600} />
-						</Card>
-					</div>
+			{layers.map((layer, layerIndex) =>
+				<ParallaxLayer
+					key={'layer_' + layerIndex}
+					offset={layer.offset}
+					speed={layer.speed}
+					onClick={() => onScroll(layer.offset + 1)}
+					style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+				>
+					{
+						layer.data.map((item, dataIndex) => {
+							switch (item.type) {
+								case 'image':
+									return <Image
+										key={'layer_' + layerIndex + '&dataIndex_' + dataIndex}
+										src={item.src}
+										width={item.width}
+										alt={''}
+										className={item.className}
+										// Make the image display full width
+									/>
+								case 'text':
+									return <div
+										key={'layer_' + layerIndex + '&dataIndex_' + dataIndex}
+										className={'border border-4 border-white bg-black/30  drop-shadow-lg text-white p-8 text-xl sm:text-3xl font-semibold backdrop-blur-lg ' + item.className}>
+										{item.text}
+									</div>
+								case 'list':
+									return <div
+										key={'layer_' + layerIndex + '&dataIndex_' + dataIndex}
+										className={'flex flex-col border border-4 border-white  drop-shadow-lg bg-black/30 text-white p-8 text-xl sm:text-3xl font-semibold backdrop-blur-lg ' + item.className}>
+										{item.list.map((li, listIndex) => <div
+											key={'layer_' + layerIndex + '&dataIndex_' + dataIndex + '&listIndex_' + listIndex}>{li}</div>)}
+									</div>
+							}
+						})
+					}
 				</ParallaxLayer>
-			))}
+			)}
 
-			{/* SECTION 2 ------------------------------------------- */}
-			<ParallaxLayer
-				sticky={{ start: 4.5, end: 8 }}
-				style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}
-			>
-				<div className={classes}>
-					<Card className={'px-6 py-4 rounded-lg text-white text-2xl font-bold drop-shadow-md'}>
-						<div>
-							Вы также сможете попасть в стаю корюшек и попытаться вместе с ними ускользнуть из рыбацких
-							сетей и перехитрить рыбаков.
-						</div>
-						<br />
-						<div>
-							Потом вы посмотрите видео об одежде рыбаков с древних времён и до настоящего времени.
-							Сможете сами составить из разных элементов новый костюм рыбака и примерить на себя рыбацкий
-							комбинезон и огромные сапоги.
-						</div>
-						<br />
-						<div>
-							В музее также представлена аутентичная лодка с парусом. С помощью видео инструкции вы
-							самостоятельно можете установить парус и отправиться на лодке в виртуальное путешествие.
-						</div>
-						<br />
-						<div>
-							Вы узнаете о подводной жизни в реке Волхов, заглянув в Волшебную бочку. И, напоследок
-							почувствуете себя художником - машинистом и создадите своё уникальное творческое
-							произведение.
-						</div>
-					</Card>
-
-					<div />
-				</div>
-			</ParallaxLayer>
-
-			{section2.map(item => (
-				<ParallaxLayer speed={item.speed} offset={item.offset} sticky={item.sticky} key={item.id}>
-					<div className={classes}>
-						<div />
-						<Card className={'flex items-center justify-center'}>
-							<Image src={item.src} alt={''} width={400} />
-						</Card>
-					</div>
-				</ParallaxLayer>
-			))}
-
-			{/*/!* SECTION 3 ------------------------------------------- *!/*/}
-			{/*<ParallaxLayer offset={5.5} style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>*/}
-			{/*	<div className={classes}>*/}
-			{/*		<Card className={'px-6 py-4 rounded-lg text-white text-2xl font-bold drop-shadow-md'}></Card>*/}
-
-			{/*		<Card className={'flex items-center justify-center'}>*/}
-			{/*			<Image src={S2P2} alt={''} width={400} />*/}
-			{/*		</Card>*/}
-			{/*	</div>*/}
-			{/*</ParallaxLayer>*/}
-
-			{/*/!* SECTION 4 ------------------------------------------- *!/*/}
-			{/*<ParallaxLayer offset={6.5} style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>*/}
-			{/*	<div className={classes}>*/}
-			{/*		<Card className={'px-6 py-4 rounded-lg text-white text-2xl font-bold drop-shadow-md'}>*/}
-			{/*			В музее также представлена аутентичная лодка с парусом. С помощью видео инструкции вы*/}
-			{/*			самостоятельно можете установить парус и отправиться на лодке в виртуальное путешествие.*/}
-			{/*		</Card>*/}
-
-			{/*		<Card className={'flex items-center justify-center'}>*/}
-			{/*			<Image src={S2P3} alt={''} width={400} />*/}
-			{/*		</Card>*/}
-			{/*	</div>*/}
-			{/*</ParallaxLayer>*/}
-
-			{/*/!* SECTION 5 ------------------------------------------- *!/*/}
-			{/*<ParallaxLayer offset={7.5} style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>*/}
-			{/*	<div className={classes}>*/}
-			{/*		<Card className={'px-6 py-4 rounded-lg text-white text-2xl font-bold drop-shadow-md'}>*/}
-			{/*			Вы узнаете о подводной жизни в реке Волхов, заглянув в Волшебную бочку. И, напоследок*/}
-			{/*			почувствуете себя художником - машинистом и создадите своё уникальное творческое произведение.*/}
-			{/*		</Card>*/}
-
-			{/*		<Card className={'flex items-center justify-center'}>*/}
-			{/*			<Image src={S2P4} alt={''} width={400} />*/}
-			{/*		</Card>*/}
-			{/*	</div>*/}
-			{/*</ParallaxLayer>*/}
 
 			{/* FOOTER ------------------------------------------- */}
-			{layersFooter.map((item, index) => (
-				<ParallaxLayer offset={item.offset} speed={item.speed} key={'parallaxLayerFooter' + index}>
-					<div className={'animation_layer parallax ' + item.id} id={item.id} />
-				</ParallaxLayer>
-			))}
+			<div onClick={() => onScroll(0)}>
+				{layersFooter.map((item, index) => (
+					<ParallaxLayer offset={item.offset} speed={item.speed} key={'parallaxLayerFooter' + index}>
+						<div className={'animation_layer parallax ' + item.id} id={item.id} />
+					</ParallaxLayer>
+				))}
+			</div>
 
 			<ParallaxLayer offset={9}>
-				<div className={'bg-[#012840] flex flex-col flex-1 h-full'} id={'footer'}>
+				<div className={'bg-[#012840] flex flex-col flex-1 h-full '} id={'footer'}>
 					FOOTER
 				</div>
 			</ParallaxLayer>
--- src/components/block_parallax/data.jsx
+++ src/components/block_parallax/data.jsx
@@ -1,6 +1,3 @@
-import Man1 from '@/shares/images/mans/man-1.png'
-import Man2 from '@/shares/images/mans/man-2.png'
-import Man3 from '@/shares/images/mans/man-3.png'
 import S1P1 from '@/shares/images/parallax/photo_2024-05-12_13-15-12.jpg'
 import S1P2 from '@/shares/images/parallax/photo_2024-05-12_13-15-13.jpg'
 import S1P3 from '@/shares/images/parallax/photo_2024-05-12_13-15-14.jpg'
@@ -8,6 +5,8 @@ import S2P1 from '@/shares/images/parallax/photo_2024-05-12_13-48-52.jpg'
 import S2P2 from '@/shares/images/parallax/photo_2024-05-12_13-15-15.jpg'
 import S2P3 from '@/shares/images/parallax/photo_2024-05-12_13-59-15.jpg'
 import S2P4 from '@/shares/images/parallax/photo_2024-05-12_13-15-19.jpg'
+import Image from 'next/image'
+import React from 'react'
 
 export const layersHeader = [
 	{
@@ -125,26 +124,153 @@ export const section2 = [
 	},
 ]
 
-export const layersFisherman = [
+export const layers = [
 	{
-		src: Man1,
-		id: 'fisherman-1',
-		offset: 6,
-		speed: 0,
-		sticky: { start: 1, end: 1.5 },
+		offset: 1,
+		speed: 0.7,
+		data: [{
+			type: 'image',
+			src: S1P1,
+			width: 800,
+			className: 'w-[90%] -translate-y-[40px] sm:w-[80%] sm:translate-x-[40px]',
+		}],
 	},
 	{
-		src: Man2,
-		id: 'fisherman-2',
-		offset: 6,
-		speed: 0,
-		sticky: { start: 2.5, end: 3 },
+		offset: 1,
+		speed: 1.5,
+		data: [{
+			type: 'text',
+			text: 'Добро пожаловать в единственный в мире интерактивный МУЗЕЙ КОРЮШКИ!',
+			className: 'w-[90%] translate-y-[180px] sm:w-[700px] sm:translate-y-[220px] sm:-translate-x-[200px]',
+		}],
+	},
+	// -----------------------------------------------------------------------
+	{
+		offset: 2,
+		speed: 0.7,
+		data: [
+			{
+				type: 'image',
+				src: S1P2,
+				className: 'w-[95%] sm:w-[70%] sm:-translate-x-[40px] ',
+				width: 800,
+			},
+		],
 	},
 	{
-		src: Man3,
-		id: 'fisherman-3',
+		offset: 2,
+		speed: 1.5,
+		data: [{
+			type: 'list',
+			list: ['В Музее вы узнаете:', '* о родственниках корюшки', '* О её друзьях и недругах', '* О том, что она любит и чему радуется.'],
+			className: 'w-[300px] w-[95%] translate-y-[200px] sm:translate-y-[280px] sm:translate-x-[220px] sm:w-[700px] ',
+		}],
+	},
+
+	// -----------------------------------------------------------------------
+	{
+		offset: 3,
+		speed: 0.5,
+		data: [{
+			type: 'image',
+			src: S1P3,
+			width: 800,
+			className: 'w-[90%] -translate-x-[5px] sm:w-[70%] sm:-translate-y-[40px] sm:translate-x-[100px]',
+		}],
+	},
+	{
+		offset: 3,
+		speed: 2,
+		data: [{
+			type: 'text',
+			text: 'В пространстве музея вас встретят уникальные экспонаты, с которыми вы можете взаимодействовать. В этом вам помогут аудиогид и видео подсказки.',
+			className: 'w-[90%] translate-y-[200px] translate-x-[10px] sm:w-[800px] sm:translate-y-[240px] sm:-translate-x-[200px]',
+		}],
+	},
+
+	// -----------------------------------------------------------------------
+	{
+		offset: 4,
+		speed: 0.7,
+		data: [{
+			type: 'image',
+			src: S2P1,
+			width: 800,
+			className: 'w-[90%] translate-x-[120px] translate-y-[40px] sm:w-[520px] sm:translate-x-[200px]',
+		}],
+	},
+	{
+		offset: 4,
+		speed: 2,
+		data: [{
+			type: 'text',
+			text: 'Вы также сможете попасть в стаю корюшек и попытаться вместе с ними ускользнуть из рыбацких сетей и перехитрить рыбаков.',
+			className: 'w-[70%] -translate-x-[50px] translate-y-[200px] sm:w-[500px] sm:-translate-x-[200px] sm:translate-y-[200px] ',
+		}],
+	},
+	// -----------------------------------------------------------------------
+	{
+		offset: 5,
+		speed: 0.7,
+		data: [
+			{
+				type: 'image',
+				src: S2P2,
+				className: 'w-[100%] -translate-y-[100px] translate-x-[-50px] sm:w-[70%] sm:-translate-x-[100px] sm:translate-y-[0rem]',
+				width: 800,
+			},
+		],
+	},
+	{
+		offset: 5,
+		speed: 1.5,
+		data: [{
+			type: 'text',
+			text: 'Потом вы посмотрите видео об одежде рыбаков с древних времён и до настоящего времени. Сможете сами составить из разных элементов новый костюм рыбака и примерить на себя рыбацкий комбинезон и огромные сапоги.',
+			className: 'w-[95%] translate-x-[5px] translate-y-[200px] sm:w-[800px] sm:translate-y-[300px] sm:translate-x-[200px]  ',
+		}],
+	},
+
+	// -----------------------------------------------------------------------
+	{
 		offset: 6,
-		speed: 0,
-		sticky: { start: 4, end: 4.5 },
+		speed: 0.5,
+		data: [{
+			type: 'image',
+			src: S2P3,
+			width: 800,
+			className: 'w-[120%] translate-y-[-50px] translate-x-[140px] sm:w-[50%] sm:translate-y-[40px] sm:translate-x-[200px]',
+		}],
+	},
+	{
+		offset: 6,
+		speed: 2,
+		data: [{
+			type: 'text',
+			text: 'В музее также представлена аутентичная лодка с парусом. С помощью видео инструкции вы самостоятельно можете установить парус и отправиться на лодке в виртуальное путешествие.',
+			className: 'w-[60%] translate-y-[180px] translate-x-[-50px] sm:w-[400px] sm:translate-y-[120px] sm:translate-x-[-280px]',
+		}],
+	},
+	// -----------------------------------------------------------------------
+	{
+		offset: 7,
+		speed: 0.7,
+		data: [
+			{
+				type: 'image',
+				src: S2P4,
+				className: 'w-[90%] translate-y-[-160px] sm:w-[70%] sm:-translate-x-[100px] sm:translate-y-[0rem]',
+				width: 800,
+			},
+		],
+	},
+	{
+		offset: 7,
+		speed: 1.5,
+		data: [{
+			type: 'text',
+			text: 'Вы узнаете о подводной жизни в реке Волхов, заглянув в Волшебную бочку. И, напоследок почувствуете себя художником - машинистом и создадите своё уникальное творческое произведение.',
+			className: 'w-[95%] translate-y-[160px] sm:translate-y-[360px] sm:translate-x-[200px] sm:w-[800px] ',
+		}],
 	},
 ]
